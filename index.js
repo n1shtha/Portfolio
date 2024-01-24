3327d8f32f7140522d62377d36fdc2f7a2568d59
Nishtha Das <nishtha3@hotmail.com>
2024-01-24 13:34:18 +0530

    added scrolling animation

--- index.js
+++ index.js
@@ -23,3 +23,17 @@ function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+let observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+let hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((e1) => observer.observe(e1));
